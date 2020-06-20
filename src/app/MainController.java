ae8304545bbcebbd565822e008da7eb6e9b5acc3
Lucas de Abreu <abreu.lucass@outlook.com>
2020-06-20 01:24:46 -0300

    trabalho finalizado 😎👉👉

--- src/app/MainController.java
+++ src/app/MainController.java
@@ -1,113 +1,214 @@
 package app;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ResourceBundle;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import program.Tipo;
 import program.Token;
 
-public class MainController {
+public class MainController implements Initializable {
 	
+	@FXML
+	private TableView<Token> tabela;
+
+	@FXML
+	private TableColumn<Token, Integer> codigoTabela;
+
+	@FXML
+	private TableColumn<Token, String> valorTabela;
+
+	@FXML
+	private TableColumn<Token, String> descricaoTabela;
+
+	@FXML
+	TextArea codigo;
 
 	private final Map<String, Tipo> PALAVRAS_RESERVADAS = new HashMap<String, Tipo>();
-	// private final ArrayList<String> PALAVRAS_RESERVADAS = new ArrayList<String>();
 	
 	private final Map<String, Tipo> CARACTERES_ESPECIAIS = new HashMap<String, Tipo>();
-	// private final ArrayList<String> CARACTERES_ESPECIAIS = new ArrayList<String>();
 	
 	private final Map<String, Tipo> OPERADORES = new HashMap<String, Tipo>();
-	// private final ArrayList<String> OPERADORES = new ArrayList<String>();
 	
 	private final Map<String, Tipo> OUTROS = new HashMap<String, Tipo>();
 	
 	private final ArrayList<Token> resultado = new ArrayList<Token>();
 	
-	@FXML
-	TextArea codigo;
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		codigoTabela   .setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getCodigo()).asObject());
+		valorTabela    .setCellValueFactory(cellData -> new SimpleStringProperty (cellData.getValue().getValor()));
+		descricaoTabela.setCellValueFactory(cellData -> new SimpleStringProperty (cellData.getValue().getDescricao()));
+	}
 	
 	public void inicia()
 	{
 		carregaTabelaDados();
 		
+		tabela.getItems().clear();
+		
 		String codigoFinalizado = codigo.getText() + "$";
 		String [] caracteres = codigoFinalizado.split("");
 		String atual = "";
+		boolean comentario = false;
+		boolean literal = false;
 		
 		for (int i = 0; i < caracteres.length; i++)
 		{
-			if (caracteres[i].equalsIgnoreCase(" ") || checkCaracterEspecial(caracteres[i]) || checkOperador(caracteres[i]) || caracteres[i].equalsIgnoreCase("$"))
-			{ 
-
-				if (!caracteres[i].equalsIgnoreCase(""))
+			
+			if ((caracteres[i].equalsIgnoreCase("(") && caracteres[i + 1].equalsIgnoreCase("*"))
+					|| caracteres[i].equalsIgnoreCase("*") && caracteres[i + 1].equalsIgnoreCase(")"))
+			{
+				if (caracteres[i].equalsIgnoreCase("("))
 				{
-					
-					String check = atual.toUpperCase();
-					check = check.trim();
-					
-					if (caracteres[i].equalsIgnoreCase(">")
-							|| caracteres[i].equalsIgnoreCase("<")
-							|| caracteres[i].equalsIgnoreCase(".")
-							|| caracteres[i].equalsIgnoreCase(":"))
-					{
-						atual = caracteres[i] + caracteres[i + 1];
-					}
-
-					if (checkOutros(atual))
-					{
-						Token token = new Token(atual, OUTROS.get(atual)); 
-						resultado.add(token);
-						atual = "";
-						i = i +2;
-					}
-					
-					if (!atual.equalsIgnoreCase(""))
-					{
-						if (checkPalavra(check))
-						{
-							Token token = new Token(atual, PALAVRAS_RESERVADAS.get(check)); 
-							resultado.add(token);
-							atual = "";
-						} 
-						else 
-						{
-							Token token = new Token(atual, Tipo.IDENTIFICADOR); 
-							resultado.add(token);
-							atual = "";
-						}
-					}
-
-					if (checkCaracterEspecial(caracteres[i]))
-					{
-						Token token = new Token(caracteres[i], CARACTERES_ESPECIAIS.get(caracteres[i])); 
-						resultado.add(token);
-					}
-					
-					if (checkOperador(caracteres[i]))
-					{
-						Token token = new Token(caracteres[i], OPERADORES.get(caracteres[i])); 
-						resultado.add(token);
-					}
+					comentario = true;
 				}
+				
+				if (caracteres[i].equalsIgnoreCase("*"))
+				{
+					comentario = true;
+					i++;
+				}
+			}
+			else if (caracteres[i].equalsIgnoreCase("\""))
+			{
+				if (literal == false)
+				{
+					literal = true;
+					continue;
+				}
+				else if (literal == true)
+				{
+					literal = false;
+					atual = "\"" + atual + "\"";
+					Token token = new Token(atual, Tipo.LITERAL); 
+					resultado.add(token);
+					atual = "";
+					continue;
+				}
+				
 			}
 			else
 			{
-				if (!caracteres[i].equalsIgnoreCase("$"))
+				if (comentario == false || literal == false)
 				{
-					atual += caracteres[i];
+					if (caracteres[i].equalsIgnoreCase(" ") || checkCaracterEspecial(caracteres[i]) || checkOperador(caracteres[i]) || caracteres[i].equalsIgnoreCase("$"))
+					{ 
+						
+						if (caracteres[i].equalsIgnoreCase("."))
+						{
+							try
+							{ 
+								int j = i;
+								do
+								{
+									j ++;
+								}
+								while (caracteres[j].equalsIgnoreCase("1") || caracteres[j].equalsIgnoreCase("2") || caracteres[j].equalsIgnoreCase("3")
+										|| caracteres[j].equalsIgnoreCase("4") || caracteres[j].equalsIgnoreCase("5") || caracteres[j].equalsIgnoreCase("6")
+										|| caracteres[j].equalsIgnoreCase("7") || caracteres[j].equalsIgnoreCase("8") || caracteres[j].equalsIgnoreCase("9")
+										|| caracteres[j].equalsIgnoreCase("0"));
+								i = j - 1;
+								atual = "";
+							}
+							catch (NumberFormatException e)
+							{
+								
+							}
+						}
+
+						if (!caracteres[i].equalsIgnoreCase(""))
+						{
+
+							String check = atual.toUpperCase();
+							check = check.trim();
+								
+							if (!atual.equalsIgnoreCase(""))
+							{
+								if (checkPalavra(check))
+								{
+									Token token = new Token(atual.trim().toUpperCase(), PALAVRAS_RESERVADAS.get(check)); 
+									resultado.add(token);
+									atual = "";
+								} 
+								else 
+								{
+									try
+									{
+										int inteiro = Integer.parseInt(atual);
+										if (inteiro > -32767 && inteiro < 32767)
+										{
+											Token token = new Token("" + inteiro, Tipo.INTEIRO); 
+											resultado.add(token);
+										}
+									}
+									catch (NumberFormatException e)
+									{
+										if (atual.length() <= 255 && !atual.equalsIgnoreCase(""))
+										{
+											Token token = new Token(atual, Tipo.IDENTIFICADOR); 
+											resultado.add(token);
+											atual = "";
+										}
+									}
+								}
+							}
+							
+							if (caracteres[i].equalsIgnoreCase(">")
+									|| caracteres[i].equalsIgnoreCase("<")
+									|| caracteres[i].equalsIgnoreCase(".")
+									|| caracteres[i].equalsIgnoreCase(":"))
+							{
+								atual = caracteres[i] + caracteres[i + 1];
+							}
+
+							if (checkOutros(atual))
+							{
+								Token token = new Token(atual, OUTROS.get(atual)); 
+								resultado.add(token);
+								atual = "";
+								i++;
+								continue;
+							}
+
+							if (checkCaracterEspecial(caracteres[i]))
+							{
+								Token token = new Token(caracteres[i], CARACTERES_ESPECIAIS.get(caracteres[i])); 
+								resultado.add(token);
+								atual = "";
+							}
+							
+							if (checkOperador(caracteres[i]))
+							{
+								Token token = new Token(caracteres[i], OPERADORES.get(caracteres[i])); 
+								resultado.add(token);
+								atual = "";
+							}
+						}
+					}
+					else
+					{
+						if (!caracteres[i].equalsIgnoreCase("$"))
+						{
+							atual += caracteres[i];
+						}
+					}
 				}
 			}
 			
 		}
 		
-		for (int i = 0; i < resultado.size(); i ++)
-		{
-			System.out.println(resultado.get(i).getValor() + " - " + resultado.get(i).getDescricao());
-		}
-
-		resultado.clear();
+		tabela.setItems(FXCollections.observableList(resultado));
 		
 	}
 	
